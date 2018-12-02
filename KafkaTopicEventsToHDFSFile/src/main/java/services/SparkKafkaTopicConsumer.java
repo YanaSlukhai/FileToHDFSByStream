4368c22aeb9e2c692602dca184b3cf3be863ed08
artemp <artemp@wix.com>
2018-12-03 01:15:22 +0200

    save in hadoop

--- KafkaTopicEventsToHDFSFile/src/main/java/services/SparkKafkaTopicConsumer.java
+++ KafkaTopicEventsToHDFSFile/src/main/java/services/SparkKafkaTopicConsumer.java
@@ -1,30 +1,33 @@
 package services;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
 import scala.Tuple2;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SparkKafkaTopicConsumer implements KafkaTopicConsumer{
     public void consumeKafkaTopic(String kafkaTopic) {
         SparkConf conf = new SparkConf().setAppName("SparkKafkaTopicConsumer").setMaster("local[*]");
-        JavaStreamingContext sparkStreamingContext = new JavaStreamingContext(conf, new Duration(10000));
+        JavaStreamingContext sparkStreamingContext = new JavaStreamingContext(conf, new Duration(100000));
         connectSparkStreamingToKafka(sparkStreamingContext, kafkaTopic);
     }
 
@@ -52,7 +55,12 @@ public class SparkKafkaTopicConsumer implements KafkaTopicConsumer{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        stream.mapToPair(record -> new Tuple2<>(record.key(), record.value())).print();
+        stream.mapToPair(record -> new Tuple2<>(record.key(), record.value())).saveAsHadoopFiles(
+                "hdfs://sandbox-hdp.hortonworks.com:6667/user/spark/stream/", "txt",
+                Text.class, IntWritable.class, TextOutputFormat.class);
+        stream.foreachRDD(rdd -> myFunction(rdd));
+
+
         System.out.println(" Printing after");
         streamingContext.start();
         try {
@@ -61,5 +69,9 @@ public class SparkKafkaTopicConsumer implements KafkaTopicConsumer{
             e.printStackTrace();
         }
 
+    }
+
+    private void myFunction(org.apache.spark.api.java.JavaRDD<ConsumerRecord<String, String>> rdd) {
+        System.out.print("s");
     }
 }
